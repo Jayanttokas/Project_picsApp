f20afb2f819e05870bfc2fa37cb80c1290f3546a
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-04-23 12:47:05 +0530

    Updates

---
+++ precache-manifest.8f219104c1458bebd5f673e45bbc13ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1026a44b584336e27220ee2878328bb",
+    "url": "/Project_picsApp/index.html"
+  },
+  {
+    "revision": "5330b247f70537c63e24",
+    "url": "/Project_picsApp/static/css/main.488961e6.chunk.css"
+  },
+  {
+    "revision": "2d4bd92103f9d0f3bcaf",
+    "url": "/Project_picsApp/static/js/2.c4df6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project_picsApp/static/js/2.c4df6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5330b247f70537c63e24",
+    "url": "/Project_picsApp/static/js/main.f2944902.chunk.js"
+  },
+  {
+    "revision": "a8bee0593f8127799d92",
+    "url": "/Project_picsApp/static/js/runtime-main.d85ad314.js"
+  }
+]);
